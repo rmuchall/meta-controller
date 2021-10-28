d7b52b84a3ffb3b6194c2f8cf6e026bdd9a2f5c8
Rob <rmuchall@protonmail.com>
2021-10-28 16:13:56 +0100

    Updated dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,8 @@ module.exports = {
     ],
     parser: "@typescript-eslint/parser",
     parserOptions: {
-        project: "./tsconfig.json"
+        tsconfigRootDir: __dirname,
+        project: "tsconfig.json",
     },
     plugins: [
         "@typescript-eslint"
